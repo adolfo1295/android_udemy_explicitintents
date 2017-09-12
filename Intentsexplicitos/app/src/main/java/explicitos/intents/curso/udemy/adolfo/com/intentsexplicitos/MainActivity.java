628b37742f2ba5bf3a8fe0cb64cc1519da262f8c
Adolfo Chavez <adolfo.dev1295@gmail.com>
2017-09-12 15:33:37 -0500

    commit inicial
    commit inicial de la aplicacion de intents eplicitos

---
+++ Intentsexplicitos/app/src/main/java/explicitos/intents/curso/udemy/adolfo/com/intentsexplicitos/MainActivity.java
@@ -0,0 +1,16 @@
+package explicitos.intents.curso.udemy.adolfo.com.intentsexplicitos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Intent explicitos
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
